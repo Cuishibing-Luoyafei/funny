a8f87bc01810b306186d883c4b74880b3a61dc95
Cuishibing <643237029@qq.com>
2018-10-04 21:30:20 +0800

    使用lombok

--- registry/src/main/java/nx/funny/registry/response/RegistryResponse.java
+++ registry/src/main/java/nx/funny/registry/response/RegistryResponse.java
@@ -1,11 +1,16 @@
 package nx.funny.registry.response;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 import nx.funny.registry.ServiceInfo;
-import nx.funny.registry.ServicePosition;
 
 import java.io.Serializable;
 import java.util.Set;
 
+@Getter
+@Setter
+@ToString
 public class RegistryResponse implements Serializable {
 
     public static final int CODE_SUCCESS = 0;
@@ -15,27 +20,4 @@ public class RegistryResponse implements Serializable {
     private String typeName;
     private Set<ServiceInfo> infos;
 
-    public int getCode() {
-        return code;
-    }
-
-    public void setCode(int code) {
-        this.code = code;
-    }
-
-    public String getTypeName() {
-        return typeName;
-    }
-
-    public void setTypeName(String typeName) {
-        this.typeName = typeName;
-    }
-
-    public Set<ServiceInfo> getInfos() {
-        return infos;
-    }
-
-    public void setInfos(Set<ServiceInfo> infos) {
-        this.infos = infos;
-    }
 }

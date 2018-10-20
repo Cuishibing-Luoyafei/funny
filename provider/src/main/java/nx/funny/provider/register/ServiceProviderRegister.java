5a9768194eb7bfd78acd76965eb972c8977be91f
Cui shibing <643237029@qq.com>
2018-10-20 20:37:47 +0800

    add ignore

--- provider/src/main/java/nx/funny/provider/register/ServiceProviderRegister.java
+++ provider/src/main/java/nx/funny/provider/register/ServiceProviderRegister.java
@@ -1,30 +1,45 @@
 package nx.funny.provider;
 
+import lombok.Getter;
+import lombok.Setter;
 import nx.funny.registry.ServiceInfo;
 import nx.funny.registry.ServicePosition;
 import nx.funny.registry.ServiceRegistry;
 import nx.funny.registry.ServiceType;
 
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
 /**
  * 解析服务提供者的信息，并注册信息到注册中心
  * 如果要注册的类有ServiceProvider注解，则优先使用注解的信息
+ *
+ * 该类应该为单例模式
  */
 public class ServiceProviderRegister {
 
+    @Getter
+    @Setter
     private ServicePositionProvider positionProvider;
 
+    @Getter
+    @Setter
     private ServiceRegistry serviceRegistry;
 
+    private Map<Class<?>,ServiceTargetFactory> typeTargetFactoryMap;
+
     public ServiceProviderRegister() {
+        typeTargetFactoryMap = new ConcurrentHashMap<>();
     }
 
     public ServiceProviderRegister(ServicePositionProvider positionProvider,
                                    ServiceRegistry serviceRegistry) {
+        this();
         this.positionProvider = positionProvider;
         this.serviceRegistry = serviceRegistry;
     }
 
-    public void register(Class<?> clazz) {
+    public <T> void register(Class<T> clazz,ServiceTargetFactory<T> targetFactory) {
         if (clazz == null)
             return;
         String name = clazz.getName();
@@ -38,10 +53,11 @@ public class ServiceProviderRegister {
             if (t != null)
                 typeName = t;
         }
-        registerWithSelfInfo(name, typeName);
+        register(name, typeName);
+        typeTargetFactoryMap.put(clazz,targetFactory);
     }
 
-    private void registerWithSelfInfo(String name, String typeName) {
+    private void register(String name, String typeName) {
         ServiceType type = new ServiceType(name, typeName);
         ServicePosition position = new ServicePosition(positionProvider.getIp(),
                 positionProvider.getPort());
@@ -49,11 +65,4 @@ public class ServiceProviderRegister {
         serviceRegistry.register(info);
     }
 
-    public void setPositionProvider(ServicePositionProvider positionProvider) {
-        this.positionProvider = positionProvider;
-    }
-
-    public void setServiceRegistry(ServiceRegistry serviceRegistry) {
-        this.serviceRegistry = serviceRegistry;
-    }
 }

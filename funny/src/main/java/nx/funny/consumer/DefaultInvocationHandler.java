96f5d47964b9040f7a10cebb0dc510034fe7d95e
Cuishibing <643237029@qq.com>
2018-11-03 21:12:39 +0800

    在调用者这边缓存服务信息

--- funny/src/main/java/nx/funny/consumer/DefaultInvocationHandler.java
+++ funny/src/main/java/nx/funny/consumer/DefaultInvocationHandler.java
@@ -1,11 +1,14 @@
 package nx.funny.consumer;
 
+import lombok.AllArgsConstructor;
 import lombok.Setter;
 import nx.funny.registry.ServiceInfo;
 import nx.funny.registry.ServiceRegistry;
 
 import java.lang.reflect.Method;
+import java.util.Map;
 import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class DefaultInvocationHandler extends SpecificInvocationHandler {
 
@@ -18,6 +21,14 @@ public class DefaultInvocationHandler extends SpecificInvocationHandler {
     @Setter
     private ServiceChooser serviceChooser;
 
+    /**
+     * 服务信息缓存的时间，0为不缓存
+     */
+    @Setter
+    private long retrieveInfoTimeInterval = 600000;
+
+    private Map<String, RetrieveTimeServiceInfoGroup> serviceInfoCache = new ConcurrentHashMap<>();
+
     public DefaultInvocationHandler(String serviceName,
                                     ServiceRegistry serviceRegistry, ServiceChooser serviceChooser) {
         this.serviceName = serviceName;
@@ -27,8 +38,26 @@ public class DefaultInvocationHandler extends SpecificInvocationHandler {
 
     @Override
     public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-        Set<ServiceInfo> serviceInfos = serviceRegistry.retrieve(serviceName);
+        Set<ServiceInfo> serviceInfos;
+        if (retrieveInfoTimeInterval <= 0) {
+            serviceInfos = serviceRegistry.retrieve(serviceName);
+        } else {
+            RetrieveTimeServiceInfoGroup retrieveTimeServiceInfoGroup = serviceInfoCache.get(serviceName);
+            if (retrieveTimeServiceInfoGroup == null ||
+                    System.currentTimeMillis() - retrieveTimeServiceInfoGroup.lastRetrieveTime > retrieveInfoTimeInterval) {
+                serviceInfos = serviceRegistry.retrieve(serviceName);
+                serviceInfoCache.put(serviceName, new RetrieveTimeServiceInfoGroup(System.currentTimeMillis(), serviceInfos));
+            } else {
+                serviceInfos = retrieveTimeServiceInfoGroup.serviceInfos;
+            }
+        }
         ServiceInfo serviceInfo = serviceChooser.choose(serviceInfos);
         return invoke(serviceInfo, method, args);
+    }
+
+    @AllArgsConstructor
+    private static class RetrieveTimeServiceInfoGroup {
+        long lastRetrieveTime = 0;
+        Set<ServiceInfo> serviceInfos;
     }
 }

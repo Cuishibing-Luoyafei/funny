e5853ca90ad255b5d3cb082dee31f85d52844882
Cui shibing <643237029@qq.com>
2018-12-01 12:48:13 +0800

    调整注册中心的构造方法

--- funny/src/main/java/nx/funny/consumer/DefaultProxyFactory.java
+++ funny/src/main/java/nx/funny/consumer/DefaultProxyFactory.java
@@ -2,6 +2,7 @@ package nx.funny.consumer;
 
 import lombok.Getter;
 import lombok.Setter;
+import nx.funny.provider.ServicePositionProvider;
 import nx.funny.registry.ServiceInfo;
 import nx.funny.registry.ServicePosition;
 import nx.funny.registry.ServiceRegistry;
@@ -14,6 +15,7 @@ import java.util.Map;
 public class DefaultProxyFactory implements ProxyFactory {
 
     @Getter
+    @Setter
     private ServiceRegistry serviceRegistry;
 
     @Setter
@@ -21,11 +23,33 @@ public class DefaultProxyFactory implements ProxyFactory {
 
     private Map<Class<?>, Object> nameProxyCache = new HashMap<>();
 
-    public DefaultProxyFactory(String registryIp, int registryPort, Class<? extends ServiceRegistry> registryType) {
-        serviceRegistry = getProxy(ServiceRegistry.class,
-                new ServiceInfo(new ServiceType(ServiceRegistry.class.getName(), registryType.getTypeName()),
-                        new ServicePosition(registryIp, registryPort)));
+    public DefaultProxyFactory() {
         serviceChooser = new RandomServiceChooser();
+    }
+
+    public DefaultProxyFactory(String registryIP, int registryPort, Class<? extends ServiceRegistry> registryType) {
+        this();
+        generateRegsitry(() -> new ServicePosition(registryIP, registryPort), registryType);
+    }
+
+    /**
+     * 根据注册中心的位置和类型生成实例对象并设置到代理工厂中
+     *
+     * @param positionProvider 注册中心位置信息
+     * @param registryType     注册中心类型,由于可能有多种注册中心类型,所以这里要指定
+     * @return 生成的注册中心实例
+     */
+    public ServiceRegistry generateRegsitry(ServicePositionProvider positionProvider,
+                                            Class<? extends ServiceRegistry> registryType) {
+        ServiceRegistry registry = getProxy(ServiceRegistry.class,
+                new ServiceInfo(
+                        new ServiceType(ServiceRegistry.class.getName(),
+                                registryType.getTypeName()),
+                        positionProvider.getServicePosition()
+                )
+        );
+        this.serviceRegistry = registry;
+        return registry;
     }
 
     @Override
@@ -33,8 +57,7 @@ public class DefaultProxyFactory implements ProxyFactory {
         return getProxy(clazz.getName(), clazz);
     }
 
-    @Override
-    public <T> T getProxy(String name, Class<T> clazz) {
+    private <T> T getProxy(String name, Class<T> clazz) {
         return (T) nameProxyCache.computeIfAbsent(clazz, s -> {
             return Proxy.newProxyInstance(Thread.currentThread().getContextClassLoader(),
                     new Class[]{s}, new DefaultInvocationHandler(name,
